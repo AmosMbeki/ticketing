6939284d13cbce2cd15304be2903fa96237add17
Amos Nyagar <amos.nyagar@jubileekenya.com>
2023-02-04 07:08:46 +0300

    Connected nats to node.js

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, {Message} from 'node-nats-streaming';
+import nats from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
+import { TicketCreatedListener } from './events/ticket-created-listener';
 
 console.clear();
 
@@ -15,19 +16,8 @@ stan.on('connect', () => {
         process.exit();
     });
 
-    const options = stan.subscriptionOptions().setManualAckMode(true).setDeliverAllAvailable().setDurableName('accounting-service');
+    new TicketCreatedListener(stan).listen();
 
-    const subscription = stan.subscribe('ticket:created', 'queue-group-name', options);
-
-    subscription.on('message', (msg: Message) => {
-        const data = msg.getData();
-
-        if(typeof data === 'string'){
-            console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-        }
-
-        msg.ack();
-    });
 });
 
 process.on('SIGINT', () => {
@@ -37,3 +27,9 @@ process.on('SIGINT', () => {
 process.on('SIGTERM', () => {
     stan.close()
 });
+
+
+
+
+
+
