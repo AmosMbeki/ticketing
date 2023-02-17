1ef1970d1c02ad80d47d2fbe95b460943d8bb41a
Amos Nyagar <amos.nyagar@jubileekenya.com>
2023-02-17 15:50:48 +0300

    test-ci added

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,12 @@
-import buildClient from "../api/build-client";
 
 const Landing = ({ currentUser }) => {
     return currentUser ? (<h1>You are signed in</h1>): (<h1>You are NOT signed in</h1>);
 };
 
-Landing.getInitialProps = async (context) => {
-    console.log('LANDING PAGE!');
-    const client = buildClient(context)
-    const { data } = await client.get('/api/users/currentuser');
+Landing.getInitialProps = async (context, client, currentUser) => {
+    
 
-    return data;    
+    return {};
 }
 
 export default Landing;
